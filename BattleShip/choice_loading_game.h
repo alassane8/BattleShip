c51b4bcbfa7ebdb76daa0ec061e166d5212deba2
alassane8 <alassane.wade@edu.ece.fr>
2023-11-18 18:55:52 +0100

    fix: disable loading empty game success

--- BattleShip/choice_loading_game.h
+++ BattleShip/choice_loading_game.h
@@ -92,22 +92,6 @@ void choix_chargement_partie(unsigned char ** mat, unsigned char ** mat_ia1, int
         fflush(stdin);
         scanf("%d", &s);
     }
-    /*
-    if (s == 3){
-        for (i = 1 ; i < 16 ; i++){
-            for (j = 1 ;j < 16 ; j++){
-                while (mat_ia1[i][j] == B){
-                    gotoligcol(25,140);
-                    SetConsoleTextAttribute(hConsole, 12);
-                    printf("Error.");
-                    SetConsoleTextAttribute(hConsole, 15);
-                    gotoligcol(25,70);
-                    fflush(stdin);
-                    scanf("%d", &s);
-                }
-            }
-        }
-    }*/
 
     if (s == 1){
         //Display decorations
@@ -124,8 +108,25 @@ void choix_chargement_partie(unsigned char ** mat, unsigned char ** mat_ia1, int
         charger_partie1(mat_ia1);
         charger_partie(mat);
         
-        //Appel de la fonction de jeux
-        jouer(mat,  mat_ia1, a, pseudo,w, choix, mat_bis, mat_ia1_bis);
+        for (i = 1 ; i < 16 ; i++){
+            for (j = 1 ;j < 16 ; j++){
+                if (mat[i][j] != B && mat_ia1[i][j] != B){
+                    //Appel de la fonction de jeux
+                    jouer(mat,  mat_ia1, a, pseudo,w, choix, mat_bis, mat_ia1_bis);
+                }
+            }
+        }
+        gotoligcol(16,70);
+        SetConsoleTextAttribute(hConsole, 12);
+        printf("Error: You loaded an empty game.");
+        gotoligcol(17,70);
+        printf("You will be redirected to the menu.");
+        SetConsoleTextAttribute(hConsole, 15);
+        sleep(4);
+        //Clean page
+        system("cls");
+        //Back menu page
+        menu(mat, mat_ia1, a , pseudo, choix, mat_bis,mat_ia1_bis);
     }
 
     if (s == 2){
@@ -143,8 +144,25 @@ void choix_chargement_partie(unsigned char ** mat, unsigned char ** mat_ia1, int
         charger_partie2(mat_ia1);
         charger_partie1_0(mat);
 
-        //Appel de la fonction de jeux
-        jouer(mat,  mat_ia1, a, pseudo,w, choix, mat_bis, mat_ia1_bis);
+        for (i = 1 ; i < 16 ; i++){
+            for (j = 1 ;j < 16 ; j++){
+                if (mat[i][j] != B && mat_ia1[i][j] != B){
+                    //Appel de la fonction de jeux
+                    jouer(mat,  mat_ia1, a, pseudo,w, choix, mat_bis, mat_ia1_bis);
+                }
+            }
+        }
+        gotoligcol(16,70);
+        SetConsoleTextAttribute(hConsole, 12);
+        printf("Error: You loaded an empty game.");
+        gotoligcol(17,70);
+        printf("You will be redirected to the menu.");
+        SetConsoleTextAttribute(hConsole, 15);
+        sleep(4);
+        //Clean page
+        system("cls");
+        //Back menu page
+        menu(mat, mat_ia1, a , pseudo, choix, mat_bis,mat_ia1_bis);
     }
 
     if (s == 3){
@@ -161,25 +179,26 @@ void choix_chargement_partie(unsigned char ** mat, unsigned char ** mat_ia1, int
         //Load matrices
         charger_partie3(mat_ia1);
         charger_partie2_0(mat);
-        /*
+
         for (i = 1 ; i < 16 ; i++){
             for (j = 1 ;j < 16 ; j++){
-                if (mat[i][j] != 'P' || mat[i][j] != 'C' || mat[i][j] != 'D' || mat[i][j] != 'S' || mat[i][j] != 'X' || mat[i][j] != 'O'){
-                    gotoligcol(16,70);
-                    printf("Error: You loaded an empty game.");
-                    gotoligcol(17,70);
-                    printf("You will be redirected to the menu.");
-                    sleep(4);
-                    //Clean page
-                    system("cls");
-                    //Back menu page
-                    menu(mat, mat_ia1, a , pseudo, choix, mat_bis,mat_ia1_bis);
+                if (mat[i][j] != B && mat_ia1[i][j] != B){
+                    //Appel de la fonction de jeux
+                    jouer(mat,  mat_ia1, a, pseudo,w, choix, mat_bis, mat_ia1_bis);
                 }
             }
-        }*/
-        
-        //Appel de la fonction de jeux
-        jouer(mat,  mat_ia1, a, pseudo,w, choix, mat_bis, mat_ia1_bis);
+        }
+        gotoligcol(16,70);
+        SetConsoleTextAttribute(hConsole, 12);
+        printf("Error: You loaded an empty game.");
+        gotoligcol(17,70);
+        printf("You will be redirected to the menu.");
+        SetConsoleTextAttribute(hConsole, 15);
+        sleep(4);
+        //Clean page
+        system("cls");
+        //Back menu page
+        menu(mat, mat_ia1, a , pseudo, choix, mat_bis,mat_ia1_bis);
     }
     if (s == 4){
         //Clean page
