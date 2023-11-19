485bc01ca595274ff2d593264e5610055b267097
alassane8 <alassane.wade@edu.ece.fr>
2023-11-19 15:36:38 +0100

    chore: add game over function

---
+++ BattleShip/game_over.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <unistd.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int game_over(unsigned char ** mat, unsigned char ** mat_ia1, int *a , char *pseudo, int choix, unsigned char ** mat_bis, unsigned char ** mat_ia1_bis){
+
+    //Variables definition
+    int B = 95;
+
+    for (int i = 1; i < 16; i++){
+        for (int j = 1; j < 16; j++){
+            if (mat_ia1[i][j] != 'X' && mat_ia1[i][j] != 'O' && mat_ia1[i][j] != B){
+                return 0;// il reste des bateaux a couler
+            }
+        }
+    }
+    return 1; // plus de bateaux a couler
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
