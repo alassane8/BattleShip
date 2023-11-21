efe8d8bb2c80783f513d393b14cc0f679860c754
alassane8 <alassane.wade@edu.ece.fr>
2023-11-22 00:23:08 +0100

    feat: include file asciiboats.h

--- BattleShip/main.c
+++ BattleShip/main.c
@@ -27,6 +27,7 @@
 #include "shot.h"
 #include "winner.h"
 #include "game_over.h"
+#include "asciiboats.h"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -77,87 +78,8 @@ int main(){
     gotoligcol(44, 70);
     printf("Copyright 2023 Battleship Alassane Wade");
 
-    srand(time(NULL));
-    choix = rand() % (2) + 1;
+    boat_decoration_main();
 
-    gotoligcol(9, 0);
-
-    if (choix == 1){
-        SetConsoleTextAttribute(hConsole, 11);
-        gotoligcol(9, 70);
-        printf("           ,_:_,\n");
-        gotoligcol(10, 70);
-        printf("             |\n");
-        gotoligcol(11, 70);
-        printf("         -*T_##_:-\n");
-        gotoligcol(12, 70);
-        printf("        i____||____i\n");
-        gotoligcol(13, 70);
-        printf("       #|==========|@\n");
-        gotoligcol(14, 70);
-        printf("       -|==========|-\n");
-        gotoligcol(15, 70);
-        printf("        |          |\n");
-        gotoligcol(16, 70);
-        printf("        |_        _|\n");
-        gotoligcol(17, 70);
-        printf("        |          |\n");
-        gotoligcol(18, 70);
-        printf("     ______________________________________________________________\n");
-        gotoligcol(19, 70);
-        printf("    |___  __   |________________  USS WADE _____________|   __  ___|\n");
-        gotoligcol(20, 70);
-        printf("        l        '-,               -   -             ,-'        /\n");
-        gotoligcol(21, 70);
-        printf("          l         l        l      ___       /      /         /\n");
-        gotoligcol(22, 70);
-        printf("            l     (l  l       l____     ____/       /  /)     /\n");
-        gotoligcol(23, 70);
-        printf("              |    V   |           l---/           |   V     |\n");
-        gotoligcol(24, 70);
-        printf("              |        |             Y             |         |\n");
-        SetConsoleTextAttribute(hConsole, 15);
-    }
-    if (choix == 2){
-        SetConsoleTextAttribute(hConsole, 11);
-        gotoligcol(8, 70);
-        printf("                                 __.#--+.__\n");
-        gotoligcol(9, 70);
-        printf("                             -==|__.----.__|==-\n");
-        gotoligcol(10, 70);
-        printf("                                 __l|  |/__\n");
-        gotoligcol(11, 70);
-        printf("                            --==|__.----.__|==--\n");
-        gotoligcol(12, 70);
-        printf("                                __/       l__\n");
-        gotoligcol(13, 70);
-        printf("                            __'{             }'__\n");
-        gotoligcol(14, 70);
-        printf("                       ___'{  ! 8     8     8 !  }'___\n");
-        gotoligcol(15, 70);
-        printf("                 ====={                               }=====\n");
-        gotoligcol(16, 70);
-        printf("                _____________________________________________\n");
-        gotoligcol(17, 70);
-        printf("               |             /   .  -- . --  .  l            |\n");
-        gotoligcol(18, 70);
-        printf("               | '--_______/    +      !      +   l______--' |\n");
-        gotoligcol(19, 70);
-        printf("               |                !             !              |\n");
-        gotoligcol(20, 70);
-        printf("               |                                             |\n");
-        gotoligcol(21, 70);
-        printf("                 l                     |                     /");
-        gotoligcol(22, 70);
-        printf("                  l_                   |                  _/");
-        gotoligcol(23, 70);
-        printf("                     l_                |               _/");
-        gotoligcol(24, 70);
-        printf("                        l_             |            _/");
-        gotoligcol(25, 70);
-        printf("                           l_          |         _/");
-        SetConsoleTextAttribute(hConsole, 15);
-    }
     gotoligcol(30, 70);
     printf("Loading...");
     sleep(3);
