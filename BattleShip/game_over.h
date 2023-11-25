f78a525e5b1435787d0d11fce68d19b84a4de412
alassane8 <alassane.wade@edu.ece.fr>
2023-11-25 14:35:36 +0100

    feat: add game over for IA

--- BattleShip/game_over.h
+++ BattleShip/game_over.h
@@ -21,3 +21,18 @@ int game_over(unsigned char ** mat, unsigned char ** mat_ia1, int *a , char *pse
     return 1; // plus de bateaux a couler
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int game_over_ia(unsigned char ** mat, unsigned char ** mat_ia1, int *a , char *pseudo, int choix, unsigned char ** mat_bis, unsigned char ** mat_ia1_bis){
+
+    //Variables definition
+    int B = 95;
+
+    for (int i = 1; i < 16; i++){
+        for (int j = 1; j < 16; j++){
+            if (mat[i][j] != 'X' && mat[i][j] != 'O' && mat[i][j] != B){
+                return 0;// il reste des bateaux a couler
+            }
+        }
+    }
+    return 1; // plus de bateaux a couler
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
